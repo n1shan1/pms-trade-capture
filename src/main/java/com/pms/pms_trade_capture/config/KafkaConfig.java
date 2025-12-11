86b108dc53698e0422d25ce91b799baf30fbd092
Nishant Dev <nishantdev03@gmail.com>
2025-12-11 13:31:15 +0000

    added proto

--- src/main/java/com/pms/pms_trade_capture/config/KafkaConfig.java
+++ src/main/java/com/pms/pms_trade_capture/config/KafkaConfig.java
@@ -1,8 +1,14 @@
 package com.pms.pms_trade_capture.config;
 
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import com.pms.pms_trade_capture.proto.TradeEventProto;
 
@@ -22,6 +28,27 @@ public class KafkaConfig {
         Map<String, Object> config = new HashMap<>();
 
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class);
+        config.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        config.put(KafkaProtobufSerializerConfig.AUTO_REGISTER_SCHEMAS, true);
+
+        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        config.put((ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
+
+        // Throughput Tuning (Batching)
+        config.put(ProducerConfig.LINGER_MS_CONFIG, 20);           // Wait up to 20ms to group messages
+        config.put(ProducerConfig.BATCH_SIZE_CONFIG, 64 * 1024);   // 64KB batch size
+        config.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+
+        return new DefaultKafkaProducerFactory<>(config);
+
+        @Bean
+        public KafkaTemplate<String, TradeEventProto> kafkaTemplate() {
+            return new KafkaTemplate<>(producerFactory());
+        }
 
 
     }
